7742ba9f2801b61ae88f1256d9acc3567429a2c4
Fernando Julian <nandokun69@gmail.com>
2023-08-21 13:38:25 +0700

    first commit

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+
+include "partials/header.php";
+require __DIR__ . '/users.php';
+
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
+
+$userId = $_GET['id'];
+deleteUser($userId);
+
+header("Location: index.php");
---
+++ view.php
@@ -0,0 +1,55 @@
+<?php
+
+include "partials/header.php";
+require __DIR__ . '/users.php';
+
+
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
+
+$userid = $_GET['id'];
+$user = getUserById($userid);
+
+if (!$user) {
+    include "partials/not_found.php";
+    exit;
+}
+
+?>
+
+<table>
+    <tbody>
+        <tr>
+            <th>Name : </th>
+            <td>
+                <?php echo $user['name']; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Username : </th>
+            <td>
+                <?php echo $user['username']; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Email : </th>
+            <td>
+                <?php echo $user['email']; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Phone : </th>
+            <td>
+                <?php echo $user['phone']; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Website : </th>
+            <td>
+                <?php echo $user['website']; ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
